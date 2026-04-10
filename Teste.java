568c57032f5096c4e6c24dee9f8824efcbbd11c0
Rafael Santos Silva <rafael.santos@pitang.com>
2026-04-09 21:40:16 -0300

    Envio da Classe Teste

---
+++ Teste.java
@@ -0,0 +1,11 @@
+
+/**
+* Classe de Teste - Engenharia de Software
+*/
+public class Teste {
+
+    public static void main(String[] args) {
+        System.out.println("Iniciando teste...");
+        System.out.println("Fim do teste.");
+    }
+}
